f31b61b078023c2444477fac25817720e6be69ec
Paulo Henrique de Sousa <paulo_henrique-16@live.com>
2024-06-28 18:08:32 -0300

    Criando e ajustando a aplicação,criando entrada de tarefas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+  const [value,useState] = useState('') 
+  
+  const erase = () => {
 
+  }
+  const submit = () => {
+    console.log('submit',value);
+    erase();
+  }
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+  const onKeyDown = (event) => {
+    if(event.which === ENTER_KEY) {
+      submit();
+    }else if(event.which === ESCAPE_KEY) {
+      erase();
+    }
+  }
+    return (
+     
+    <section id='app' className='container'>
+      <header>
+        <h1 className='title'>Faça Tarefas</h1>
+      </header>
+      <section className='main'>
+        <input 
+        className='new-todo' 
+        placeholder='O que precisa ser feito'
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown} />
+      </section>
+
+    </section>
+
+
+  )
+}
 export default App;
